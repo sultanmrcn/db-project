86f3e04df88a49739caf93c3d747caa3b65fb351
sultanmercan <sultanmercann@gmail.com>
2023-03-10 01:38:33 +0100

    like practice

--- Queries/Day1_SQL.sql
+++ Queries/Day1_SQL.sql
@@ -49,9 +49,39 @@ select * from EMPLOYEES where EMPLOYEE_ID = 135 or EMPLOYEE_ID =176 or EMPLOYEE_
 -- get me all info where employee id 135 176 154 129
 select * from EMPLOYEES where EMPLOYEE_ID  in (135,176,154,129) ;
 
-
 --get me the city of where country id IT,US,UK
 select  CITY, COUNTRY_ID from LOCATIONS  WHERE COUNTRY_ID IN  ('IT', 'US','UK');
+
+
+---- COUNT-------
+select COUNT (*) from EMPLOYEES where JOB_ID ='IT_PROG' OR JOB_ID ='SA_REP';
+-- how many employees are working as IT_prog or SA_REP
+
+
+select count (*) from EMPLOYEES where SALARY >8000;
+--HOW MANY EMPLOYEE making more than 8000
+
+SELECT  COUNT (DISTINCT FIRST_NAME)  FROM EMPLOYEES ;
+-- how many unique first names we have?
+
+select * from EMPLOYEES ORDER BY SALARY DESC;
+--get me all employees information based on who is making more salary to low salary
+
+select * from EMPLOYEES ORDER BY FIRST_NAME ASC;
+--get me all employees information order by alphabetical based on first name
+
+select * from EMPLOYEES WHERE  FIRST_NAME LIKE 'C%';
+--get me all employees whose first name starts with C
+
+select * from EMPLOYEES WHERE  FIRST_NAME LIKE '_u%';
+
+
+
+
+
+
+
+
 
 
 
